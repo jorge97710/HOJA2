58fffb1f67523a89906eda1025b3daa316e701d8
jorge97710 <jazmitia_10@hotmail.com>
2016-07-20 11:22:59 -0600

    V 0.1.1 se agregan las 3 clases que se utilizaran

--- HOJA2/src/main.java
+++ HOJA2/src/main.java
@@ -1,4 +1,13 @@
 
 public class main {
+	 public static void main(String[] args) {
+		    Stack<String> miStack = new StackVector<String>();
+		    miStack.push("Adios");  //1
+			miStack.push("Hola");   //2
 
+		    String cadena = miStack.pop();
+		    System.out.println(cadena);
+			cadena = miStack.pop();
+			System.out.println(cadena);
+		  }
 }
